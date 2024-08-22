ef90a9b66672d48a8e43b2c536eb70739e6096df
solmsl <mejiasilesol@gmail.com>
2024-08-22 00:32:12 -0300

    cambios en las rutas

--- jjDeportes/server/app.js
+++ jjDeportes/server/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// Confuguración del middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const usuario=require('./rutas/user');
-app.use('/user',usuario)
+
+app.use('/user',usuario);
+
 // Ruta INICIO
 app.get('/', (req, res) => {
-  res.send('Bienvenidos al inicio de la página RIAB');
+  res.send('Bienvenidos al inicio de la página');
 });
+
 app.listen(3000, () => {
   console.log('Mi aplicacion esta funcionando en el puerto 3000!');
 })
