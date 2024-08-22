0717a3e67edc68c5db4e4b99b70a004255e4583a
solmsl <mejiasilesol@gmail.com>
2024-08-22 00:34:09 -0300

    espacios en el archivo

--- jjDeportes/server/rutas/user.js
+++ jjDeportes/server/rutas/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controladores/user')
+
+const userController = require('../controladores/user');
+
 router.get('/', userController.showUsers);
-router.post('/crear',userController.createUser)
+router.post('/crear',userController.createUser);
+
 module.exports = router
