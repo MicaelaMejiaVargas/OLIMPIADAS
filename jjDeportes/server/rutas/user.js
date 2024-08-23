749e6f8f75f823bf1c0a5856e2b226d25b02d74d
solmsl <mejiasilesol@gmail.com>
2024-08-23 19:16:48 -0300

    cambio de nombres a los archivos de user del controlador

--- jjDeportes/server/rutas/user.js
+++ jjDeportes/server/rutas/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const userController = require('../controladores/user');
+const userController = require('../controladores/user_controlador');
 
 router.get('/', userController.showUsers);
 router.post('/',userController.createUser);
