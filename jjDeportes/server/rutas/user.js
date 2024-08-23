857fb18498e4f71d714a50faa5a0d41809d0b39a
solmsl <mejiasilesol@gmail.com>
2024-08-22 21:23:56 -0300

    cambio en la ruta

--- jjDeportes/server/rutas/user.js
+++ jjDeportes/server/rutas/user.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const userController = require('../controladores/user');
 
 router.get('/', userController.showUsers);
-router.post('/crear',userController.createUser);
+router.post('/',userController.createUser);
 
 module.exports = router
