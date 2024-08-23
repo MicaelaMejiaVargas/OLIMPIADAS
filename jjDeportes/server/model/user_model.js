6f1839d1862bb96aa5754382183956e9501980a2
solmsl <mejiasilesol@gmail.com>
2024-08-23 18:44:40 -0300

    cambio en las validaciones

--- jjDeportes/server/model/user_model.js
+++ jjDeportes/server/model/user_model.js
@@ -2,18 +2,52 @@ const { Sequelize } = require('sequelize');
 const db = require('../config/db_sequelize');
 
 const Usuario = db.define('usuario', {
-    correo: {
-        type: Sequelize.STRING,
-        primaryKey: true
-    },
     username:{
-        type:Sequelize.STRING
+        type:Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          len: {
+            args: [3, 50],
+            msg: 'Nombre inválido: debe tener entre 3 y 50 caracteres'
+          },
+          notEmpty: true
+      }
+    },
+    correo: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+      allowNull: false,
+      validate: {
+        isEmail:{
+          msg: "Formato de email inválido."
+        },
+        notEmpty: true
+      }
     },
     passw:{
-        type:Sequelize.STRING
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        len: {
+          args: [8, 100],
+          msg: "La contraseña debe tener al menos 8 caracteres."
+        },
+        notEmpty: true,
+        is: {
+          args: /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[\W_]).{8,}$/,
+          msg: "La contraseña debe contener al menos una mayúscula, una minúscula, un número y un carácter especial."
+        }
+      }
     },
     accType:{
-        type:Sequelize.BOOLEAN
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      validate: {
+        isIn: {
+          args: [[true, false]],
+          msg: "El tipo de cuenta debe ser verdadero o falso."
+        }
+      }
     }
 },{
     timestamps:false
