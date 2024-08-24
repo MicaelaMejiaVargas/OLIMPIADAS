06cbacc181f3357d8bc1d08465327f38eec50627
solmsl <mejiasilesol@gmail.com>
2024-08-23 23:49:15 -0300

    nuevos rutas

--- jjDeportes/server/controladores/user_controlador.js
+++ jjDeportes/server/controladores/user_controlador.js
@@ -11,22 +11,91 @@ const showUsers=async (req,res)=>{
 
 const createUser=async(req,res)=>{
     try {
-        const {correo,username,passw,accType}=req.body;
+        const {username,correo,passw,accType}=req.body;
         const nuevoUsuario= await usuario.create({ 
-            correo,username,passw,accType
+            username,correo,passw,accType
         });
         nuevoUsuario.save();
         return res.status(200).json({
             message: "usuario creado!",
             data: nuevoUsuario
         })
-        // const user
+        
     } catch (error) {
         return res.json({err: error})
+        // return res.status(500).json({error: "Internal Server Error"})
     }
 }
 
+const updateUser = async (req, res) => {
+    try {
+      const pasarCorreo = req.params.correo;
+      const {username, correo, passw, accType} = req.body;
+  
+      if (!username || username.length < 3 || username.length >50) { 
+        return res.status(401).json({error: "User inválido"})
+      }
+      if (!correo || correo.length < 3) {
+        return res.status(401).json({error: "Nombre inválido"})
+      }
+      if (!apellido || apellido.length < 3) {
+        return res.status(401).json({error: "Apellido inválido"})
+      }
+      if (isNaN(telefono)) {
+        return res.status(401).json({error: "Número de Telefono inválido"})
+      }
+      
+      const buscarUsuario = await usuario.findOne({ where: { correo: pasarCorreo } });
+  
+      if(!buscarUsuario){
+        return res.status(400).json({
+          message: "Usuario no encontrado."
+        });
+      }
+  
+      const actUser = await buscarUsuario.update({username, passw, accType});
+      
+      return res.status(200).json({
+        message: "Usuario actualizado!",
+        data: actUser
+      })
+    } catch (error) {
+      return res.status(500).json({error: "Internal Server Error"})
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const correo= req.params.correo
+        const buscarUser = await user.findOne({where: {correo}});
+
+        if(!buscarUser){
+        return res.status(404).json({ message: "User no encontrado."});
+        }
+
+        const borrarUser = await buscarUser.destroy();
+        return res.status(200).json({
+            message: "Usuario Borrado con exito!",
+            data: borrarUser
+        })
+    } catch (error) {
+        return res.status(500).json({error: "Internal Server Error"})
+    }
+}
+const showOneUser = async (req, res) => {
+    try {
+      const { correo } = req.params
+      const user = await user.findByPk(correo)
+    
+      return res.status(200).json(user) 
+    } catch (error) {
+      return res.status(500).json({error: "Internal Server Error"})
+    }
+  }
 module.exports={
     showUsers,
-    createUser
+    createUser,
+    updateUser,
+    deleteUser,
+    showOneUser
 }
