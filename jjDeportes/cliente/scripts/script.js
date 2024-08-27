bcad17884d7d6766750fc2d33214785f55983f4f
solmsl <mejiasilesol@gmail.com>
2024-08-26 22:33:12 -0300

    FUNCIONA CARRITO

--- jjDeportes/cliente/scripts/script.js
+++ jjDeportes/cliente/scripts/script.js
@@ -1,30 +1,36 @@
 function cargarProducto(posicion){
 
-    const carrito = document.querySelector("#carrito");
+    const carrito = document.querySelector("#prod_select");
     const div = document.createElement("div");
 
     const textoInfo = document.getElementById("textoInfo");
-    const btnVaciar = document.querySelector(".noVisible")
-
-    if(textoInfo != null){
-        textoInfo.remove();
-        btnVaciar.style.display = "block";
-    }
+    const btnVaciar = document.querySelector(".noVisible");
+    const total = document.querySelector(".total");
 
     alert("Producto comprado!!")
     div.innerHTML = `
-    <h4>${producto[posicion].nombre}</h4>
-    <h4>${producto[posicion].precio}</h4>
+    <h5>${producto[posicion].nombre} $${producto[posicion].precio}</h5>
     `
-    carrito.appendChild(div);
+    carrito.append(div);
+
+    if(textoInfo != null){
+        textoInfo.remove();
+        // total.remove();
+        btnVaciar.style.display = "block";
+    }
+
 }
 
 function vaciarCarrito() {
     const carrito = document.getElementById("carrito");
     carrito.innerHTML = `
-        <p id="textoInfo">Carrito vacio 😢</p> 
+        <div id="prod_select"></div>
+            <p id = "textoInfo">Carrito vacio 😢</p>
+        <div class="total">
+            <p>Total: <span class="total-price"> $0</span>Ars</p>
+        </div>
         <div class="noVisible">
-            <button onclick="vaciarCarrito()" type="button" class="btn btn-danger">Vaciar</button>
+            <button onclick="vaciarCarrito()" type="button" class="btn btn-danger">Vaciar Carrito</button>
         </div>
     `
 }
@@ -33,51 +39,31 @@ let producto = [ //Estrutura Array
     { //Elemento 1 | posicion 0
         nombre: "ZAPATILLA",
         precio: 13000,
-        marca: "",
-        talle: "M",
-        color: "Celeste",
-        stock: 6,
-        descripcion: "...",
         id: 1 //Dato clave 
     },
     { //Elemento 2 | posicion 1
         nombre: "CAMPERA",
         precio: 12500,
-        marca: "",
-        talle: "S",
-        color: "Verde",
-        stock: 16,
-        descripcion: "...",
         id: 2
     },
     { //Elemento 3 | posicion 2
         nombre: "RAQUETA",
         precio: 10500,
-        marca: "",
-        talle: "M",
-        color: "azul",
-        stock: 16,
-        descripcion: "...",
         id: 3
     },
     { //Elemento 4 | posicion 3
         nombre: "CUERDAS",
-        precio: 12000,
-        marca: "",
-        talle: "M",
-        color: "azul",
-        stock: 16,
-        descripcion: "...",
+        precio: 1000,
         id: 4
     },
     { //Elemento 5 | posicion 4
         nombre: "MEDIAS",
         precio: 12000,
-        marca: "",
-        talle: "M",
-        color: "azul",
-        stock: 16,
-        descripcion: "...",
-        id: 4
+        id: 5
+    },
+    { //Elemento 6 | posicion 5
+        nombre: "PELOTAS",
+        precio: 12000,
+        id: 6
     }
 ]
